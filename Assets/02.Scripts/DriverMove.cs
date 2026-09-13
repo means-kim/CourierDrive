b933cdc3ed33779455a993cd8d5736d23f488650
김민수 <kms99400@gmail.com>
2026-09-13 13:08:46 +0900

    ✨ Feat: 드라이버 이동 구현
    - 상하 키보드 키를 누르면 드라이버가 앞,뒤로 이동
    - 좌우 키보드 키를 누르면 드라이버가 그쪽 방향으로 회전

---
+++ Assets/02.Scripts/DriverMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DriverMove : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float  _moveSpeed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * _rotationSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+        
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
